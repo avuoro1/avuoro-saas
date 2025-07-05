5a941e44cb7584f295cfb3002451775d1eac21cd
avuoro1 <avuoro1@gmail.com>
2025-07-05 15:52:14 -0500

    index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "4rem", textAlign: "center" }}>
+      <h1>Welcome to Avuoro 👋</h1>
+      <p>This is the beginning of something massive.</p>
+    </div>
+  );
+}
